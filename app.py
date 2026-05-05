4587f7f91ac1438f1b62d42979d519b59f6700ba
chr0nzz <187675356@users.noreply.github.com>
2026-05-05 08:57:39 -0400

    v1.0.3: fix save middleware silently failing on some browsers (#37)

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from io import StringIO
 from cryptography.fernet import Fernet, InvalidToken
 
 GITHUB_REPO  = "chr0nzz/traefik-manager"
-APP_VERSION  = "1.0.2"
+APP_VERSION  = "1.0.3"
 
 
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
