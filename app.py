82f907724eeca24edbee0faecb6d64b210c19163
chr0nzz <187675356@users.noreply.github.com>
2026-05-06 11:00:32 -0400

    fix OIDC callback URL uses http instead of https behind reverse proxy (#40)

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from functools import wraps
 import click
 from flask import (Flask, render_template, request, redirect,
                    url_for, flash, jsonify, abort, session, send_file)
+from werkzeug.middleware.proxy_fix import ProxyFix
 from ruamel.yaml import YAML
 from ruamel.yaml import YAML as SafeYAML
 from io import StringIO
@@ -30,6 +31,7 @@ logger = logging.getLogger("traefik-manager")
 
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 _CONFIG_DIR      = os.path.dirname(os.environ.get('SETTINGS_PATH', '/app/config/manager.yml'))
 _SECRET_KEY_PATH = os.path.join(_CONFIG_DIR, '.secret_key')
